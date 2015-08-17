3d1f693377f550d787d300751f6939aba973aaaf
ye900424 <cao890809@163.com>
2015-08-17 16:47:02 +0800

    123
    21

---
+++ src/main/java/com/bolo/examples/base/entity/UserInfo.java
@@ -0,0 +1,68 @@
+package com.bolo.examples.base.entity;
+
+import java.util.Date;
+
+public class UserInfo {
+
+	private String userId;
+	private String userName;
+	private String password;
+	private String mobileNo;
+	private String nickName;
+	private String userPic;
+	private String email;
+	private Date createTime;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getUserPic() {
+		return userPic;
+	}
+	public void setUserPic(String userPic) {
+		this.userPic = userPic;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	
+	
+	
+	
+}
